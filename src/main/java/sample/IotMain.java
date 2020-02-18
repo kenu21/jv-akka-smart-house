2631e8444a08902443fa6d6a16711c632c97f3ec
Iurii Keniu <kenu21@gmail.com>
2020-02-18 18:11:07 +0200

    added mainActor

---
+++ src/main/java/sample/IotMain.java
@@ -0,0 +1,10 @@
+package sample;
+
+import akka.actor.typed.ActorSystem;
+
+public class IotMain {
+    public static void main(String[] args) {
+        // Create ActorSystem and top level supervisor
+        ActorSystem.create(IotSupervisor.create(), "iot-system");
+    }
+}
